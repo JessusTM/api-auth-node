820694156dbc9330de25dc4698b63d0f26d1077b
TM_Jesus <j.tapia05@ufromail.cl>
2024-12-06 19:01:45 -0300

    feat(view): agregar ruta de logout con autorización
    - Se añade una nueva ruta «/logout» que requiere autorización para cerrar sesión en la aplicación.

--- src/view/userView.ts
+++ src/view/userView.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controller/userController";
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+} from "../controller/userController";
+import { authorization } from "../middleware/authorization";
 
 export const userView = () => {
   const router = Router();
   router.post("/register", registerUser);
   router.post("/login", loginUser);
+  router.post("/logout", authorization as any, logoutUser);
   return router;
 };
