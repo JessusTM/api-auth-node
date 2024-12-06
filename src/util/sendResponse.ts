7ba607851e4d4efcfedeabe04285082a0e53466b
TM_Jesus <j.tapia05@ufromail.cl>
2024-12-06 16:33:42 -0300

    feat(util): agregar función «sendResponse» para respuestas estandarizadas en los controladores
    - Se implementa la función «sendResponse» en el módulo «src/util/sendResponse.ts» para centralizar la creación de respuestas HTTP estandarizadas.
          - La función recibe los parámetros «res», «status», «message» y opcionalmente «data». Con esto genera una respuesta JSON con un formato consistente.

---
+++ src/util/sendResponse.ts
@@ -0,0 +1,14 @@
+import { Response } from "express";
+
+export const sendResponse = (
+  res: Response,
+  status: number,
+  message: string,
+  data?: any,
+) => {
+  res.status(status).json({
+    status: status === 200 || status === 201 ? "Éxito" : "Error",
+    message,
+    ...data,
+  });
+};
