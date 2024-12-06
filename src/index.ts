10fc5135add7ca33eca53a45ed12292132e51881
TM_Jesus <j.tapia05@ufromail.cl>
2024-12-06 18:57:52 -0300

    feat(index): agregar middleware cookie-parser
    - Se añade el middleware «cookie-parser» a la aplicación para el manejo de cookies.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { userView } from "./view/userView";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser());
 
 dotenv.config();
 
